d484014424086892956a2f7481ebcd24a3bc3252
Lucas <lucasagnez@hotmail.com>
2020-06-17 15:44:11 -0300

    Add A03

--- A03.cpp
+++ A03.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]){
 			return 1;
 		}
 		std::string mensagem;
-		std::ofstream arquivo_saida("arquivo", std::ios::app);
+		std::ofstream arquivo_saida("arquivo", std::ios::out);
 		std::cout << "Insira sua Mensagem: " << std::endl;
 		std::getline(std::cin, mensagem);
 		arquivo_saida << mensagem;
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]){
 		std::cout << "Uso: " << argv[0] << " add <'mensagem'>" << std::endl;
 		return 1;
 	}
-	std::ofstream arquivo_saida("arquivo", std::ios::app);
+	std::ofstream arquivo_saida("arquivo", std::ios::out);
 	arquivo_saida << argv[2];
 	arquivo_saida.close();
 	std::cout << "Arquivo Salvo Com Sucesso" << std::endl;
